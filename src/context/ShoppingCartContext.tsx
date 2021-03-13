41aeb78ba2b3651ab99dfd5f755191454221b561
Victor <victor.as.nascimento@gmail.com>
2021-03-12 21:50:44 -0300

    adding shopping context and product requests

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,9 @@
 import { createContext, useState, ReactNode } from 'react';
 
+import {getProducts} from '../repositories/productsRepository';
+
+import * as dummy from '../../dummy.json';
+
 interface ShoppingCartProviderProps {
     children: ReactNode;
 }
@@ -7,16 +11,20 @@ interface ShoppingCartProviderProps {
 export const ShoppingCartContext = createContext({})
 
 export const ShoppingCartProvider = ({ children }: ShoppingCartProviderProps) => {
-    const [products, setProducts] = useState([
-        {
-            banana: {
-                weight: 0,
-                price: 10
-        }}
-    ]);
+    const [products, setProducts] = useState([]);
     const [subtotal, setSubtotal] = useState(234);
     const [discount, setDiscount] = useState(1);
     const [total, setTotal] = useState(243);
+
+    const availableProducts = getProducts()
+        .then(res => {
+            console.log(res)
+            setProducts(res)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    console.log(dummy, availableProducts, products)
 
     return (
         <ShoppingCartContext.Provider
---
+++ src/repositories/productsRepository.ts
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+export const getProducts = () => {
+    return axios.get('https://shielded-wildwood-82973.herokuapp.com/products.json')
+        .then(async res => {
+            const response = await res.data.products;
+            return response
+        })
+        .catch(async err => {
+            const error = await {status: err.response.status, message: err.response.statusText}
+            console.error(error)
+            return error
+        })
+}
